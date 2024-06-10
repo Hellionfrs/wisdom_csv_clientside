960287b58aca6e51a90422efd8a7211b72996b33
fred <fredyrs.wdev@gmail.com>
2024-06-10 17:10:07 -0500

    feat: add nice board for login/register pages using shadcn components

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,6 +3,11 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { useAuth } from '@/context/AuthContext';
+import { Avatar, AvatarImage } from '@/components/ui/avatar';
+import { AvatarFallback } from '@radix-ui/react-avatar';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 
 const schema = z.object({
   email: z.string().email({ message: 'Email inválido' }),
@@ -27,23 +32,40 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-md p-8 space-y-4 bg-white shadow-md rounded-lg">
-        <h2 className="text-2xl font-bold text-center">Iniciar Sesión</h2>
-        <div>
-          <label>Email</label>
-          <input {...register('email')} type="email" className="border p-2 w-full" />
-          {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+        <div className="flex h-screen rounded-lg shadow-md m-16">
+          <div className="w-1/2 flex flex-col gap-4 bg-blue-600 h- justify-between rounded-l-lg text-white">
+            <header className='flex gap-2 text-start p-4'>
+                <Avatar>
+                    <AvatarImage src="/src/assets/WISDOMIA-ACTUALIZADO.png"/>
+                    <AvatarFallback>Wisdomia</AvatarFallback>
+                </Avatar>
+                <h1 className="text-2xl font-bold text-center self-center">Wisdomia</h1>
+
+            </header>
+            <div className='justify-self-end'>
+                <blockquote className="italic text-xl mb-4">“Gracias por ayudarnos a mejorar la educacion en el Peru!”</blockquote>
+                {/* <p className="mt-4">- Team</p> */}
+            </div>
+            
+          </div>
+          <div className="w-1/2 flex flex-col justify-center items-center">
+            <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-md p-8 space-y-4 bg-white ">
+              <h2 className="text-2xl font-bold text-center">Iniciar Sesión</h2>
+              <div>
+                <Label>Email</Label>
+                <Input {...register('email')} type="email" className="border p-2 w-full" />
+                {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+              </div>
+              <div>
+                <Label>Contraseña</Label>
+                <Input {...register('password')} type="password" className="border p-2 w-full" />
+                {errors.password && <p className="text-red-500">{errors.password.message}</p>}
+              </div>
+              <Button type="submit" className="p-2 w-full">Iniciar Sesión</Button>
+            </form>
+          </div>
         </div>
-        <div>
-          <label>Contraseña</label>
-          <input {...register('password')} type="password" className="border p-2 w-full" />
-          {errors.password && <p className="text-red-500">{errors.password.message}</p>}
-        </div>
-        <button type="submit" className="bg-blue-500 text-white p-2 w-full">Iniciar Sesión</button>
-      </form>
-    </div>
-  );
-};
+      );
+    };
 
 export default LoginPage;
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '@/context/AuthContext';
+import { Avatar, AvatarImage } from '@/components/ui/avatar';
+import { AvatarFallback } from '@radix-ui/react-avatar';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 
 const schema = z.object({
   email: z.string().email({ message: 'Email inválido' }),
@@ -31,26 +36,40 @@ const RegisterPage: React.FC = () => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-md p-8 space-y-4 bg-white shadow-md rounded-lg">
-        <h2 className="text-2xl font-bold text-center">Registrate</h2>
-        <div>
-          <label>Email</label>
-          <input {...register('email')} type="email" className="border p-2 w-full" />
-          {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+    <div className="flex h-screen rounded-lg shadow-md m-16">
+      <div className="w-1/2 flex flex-col justify-center items-center">
+        <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-md p-8 space-y-4 bg-white">
+          <h2 className="text-2xl font-bold text-center">Crea tu usuario</h2>
+          <div>
+            <Label>Email</Label>
+            <Input {...register('email')} type="email" className="border p-2 w-full" />
+            {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+          </div>
+          <div>
+            <Label>Contraseña</Label>
+            <Input {...register('password')} type="password" className="border p-2 w-full" />
+            {errors.password && <p className="text-red-500">{errors.password.message}</p>}
+          </div>
+          <div>
+            <Label>Confirmar Contraseña</Label>
+            <Input {...register('confirmPassword')} type="password" className="border p-2 w-full" />
+            {errors.confirmPassword && <p className="text-red-500">{errors.confirmPassword.message}</p>}
+          </div>
+          <Button type="submit" className="p-2 w-full">Registrar</Button>
+        </form>
+      </div>
+      <div className="w-1/2 flex flex-col gap-4 bg-blue-600 justify-between rounded-r-lg text-white">
+        <header className='flex gap-2 p-4 self-end'>
+            <Avatar>
+                <AvatarImage src="/src/assets/WISDOMIA-ACTUALIZADO.png"/>
+                <AvatarFallback>Wisdomia</AvatarFallback>
+            </Avatar>
+            <h1 className="text-2xl font-bold text-center self-center">Wisdomia</h1>
+        </header>
+        <div className='justify-self-end'>
+            <blockquote className="italic text-xl mb-4">“Gracias por ayudarnos a mejorar la educación en el Perú!”</blockquote>
         </div>
-        <div>
-          <label>Contraseña</label>
-          <input {...register('password')} type="password" className="border p-2 w-full" />
-          {errors.password && <p className="text-red-500">{errors.password.message}</p>}
-        </div>
-        <div>
-          <label>Confirmar Contraseña</label>
-          <input {...register('confirmPassword')} type="password" className="border p-2 w-full" />
-          {errors.confirmPassword && <p className="text-red-500">{errors.confirmPassword.message}</p>}
-        </div>
-        <button type="submit" className="bg-blue-500 text-white p-2 w-full">Registrar</button>
-      </form>
+      </div>
     </div>
   );
 };
